a9cee0c7349b30d6e76b9c32ab8aad480b709fda
fedepvg <federicopvg@gmail.com>
2020-11-02 19:52:55 -0300

    agregado algoritmo a star

---
+++ Parcial2-IA/Assets/Scripts/MGrid.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MGrid : MonoBehaviour
+{
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	public Vector2 currentNode;
+
+	Node[,] grid;
+	BoxCollider boxVolume;
+	float nodeDiameter;
+	int gridSizeX;
+	int gridSizeY;
+
+	void Awake()
+	{
+		nodeDiameter = nodeRadius * 2;
+		boxVolume = GetComponent<BoxCollider>();
+		CreateGrid();
+	}
+
+	void CreateGrid()
+	{
+		Vector3 worldBottomLeft = new Vector3(boxVolume.bounds.min.x, boxVolume.bounds.max.y, boxVolume.bounds.min.z);
+
+		gridSizeX = Mathf.RoundToInt(boxVolume.bounds.size.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(boxVolume.bounds.size.z / nodeDiameter);
+
+		grid = new Node[gridSizeX, gridSizeY];
+
+		for (int x = 0; x < gridSizeX; x++)
+		{
+			for (int y = 0; y < gridSizeY; y++)
+			{
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = Random.Range(0f, 1f) < 0.85f ? true : false;
+				grid[x, y] = new Node(walkable, worldPoint, x, y);
+			}
+		}
+	}
+
+	public List<Node> GetNeighbours(Node node)
+	{
+		List<Node> neighbours = new List<Node>();
+
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				if (x == 0 && y == 0)
+					continue;
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+				{
+					neighbours.Add(grid[checkX, checkY]);
+				}
+			}
+		}
+
+		return neighbours;
+	}
+
+
+	public Node GetNodeFromWorldPosition(Vector3 worldPosition)
+	{
+		Vector3 worldBottomLeft = new Vector3(boxVolume.bounds.min.x, boxVolume.bounds.max.y, boxVolume.bounds.min.z);
+		Vector3 worldTopRight = new Vector3(boxVolume.bounds.max.x, boxVolume.bounds.max.y, boxVolume.bounds.max.z);
+
+		float maxX = worldTopRight.x - worldBottomLeft.x;
+		float maxY = worldTopRight.z - worldBottomLeft.z;
+
+		float yPerc = (worldPosition.z - worldBottomLeft.z - nodeRadius) / maxY;
+		float xPerc = (worldPosition.x - worldBottomLeft.x - nodeRadius) / maxX;
+
+		int x = Mathf.Clamp(Mathf.RoundToInt((gridSizeX) * xPerc), 0, gridSizeX - 1);
+		int y = Mathf.Clamp(Mathf.RoundToInt((gridSizeY) * yPerc), 0, gridSizeY - 1);
+
+		return grid[x, y];
+	}
+}
---
+++ Parcial2-IA/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Path
+{
+	public bool pathExists;
+	public Node start;
+	public Node target;
+	public List<Node> nodeList;
+}
+
+public class Pathfinding : MonoBehaviour
+{
+	public Transform seeker, target;
+	MGrid grid;
+	public Vector2 currentStart;
+	public Vector2 currentTarget;
+
+	void Awake()
+	{
+		grid = GetComponent<MGrid>();
+	}
+
+	void Update()
+	{ 
+		//if(Input.GetKeyDown(KeyCode.Space))
+			FindPath(seeker.position, target.position);
+	}
+
+	Path FindPath(Vector3 startPos, Vector3 targetPos)
+	{
+		Path currentPath = new Path();
+
+		Node startNode = grid.GetNodeFromWorldPosition(startPos);
+		Node endNode = grid.GetNodeFromWorldPosition(targetPos);
+
+		currentPath.start = startNode;
+		currentPath.target = endNode;
+
+		List<Node> openList = new List<Node>();
+		List<Node> closedList = new List<Node>();
+
+		openList.Add(startNode);
+
+		Node currentNode = startNode;
+
+		while (openList.Count > 0)
+		{
+			float lowestCost = Single.MaxValue;
+
+			foreach(Node node in openList)
+            {
+				if(node.fCost < lowestCost)
+                {
+					lowestCost = node.fCost;
+					currentNode = node;
+                }
+            }
+
+			openList.Remove(currentNode);
+			closedList.Add(currentNode);
+
+			if (currentNode == endNode)
+			{
+				currentPath.nodeList = new List<Node>(RetracePath(startNode, endNode));
+				currentPath.pathExists = true;
+				return currentPath;
+			}
+
+			foreach(Node node in grid.GetNeighbours(currentNode))
+            {
+				if (closedList.Contains(node) || !node.walkable)
+					continue;
+				node.gCost = GetDistance(currentNode, node);
+				node.hCost = GetDistance(node, endNode);
+				node.parent = currentNode;
+				if (!openList.Contains(node))
+					openList.Add(node);
+            }
+		}
+
+		currentPath.pathExists = false;
+		return currentPath;
+	}
+
+	List<Node> RetracePath(Node startNode, Node endNode)
+	{
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode)
+		{
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Add(startNode);
+		path.Reverse();
+
+		return path;
+	}
+
+	float GetDistance(Node nodeA, Node nodeB)
+	{
+		return Vector3.Distance(nodeA.worldPosition, nodeB.worldPosition);
+	}
+}
